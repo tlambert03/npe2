e4e7c5b022264779cca733ab257d969b761a9976
Talley Lambert <talley.lambert@gmail.com>
2021-12-03 20:55:51 -0500

    fix test warnings

--- .github/workflows/test_plugin.py
+++ .github/workflows/test_plugin.py
@@ -1,32 +1,33 @@
-import configparser
 import os
+import warnings
 from importlib import metadata
 from typing import Iterable, Tuple
 
-PLUGIN = os.getenv("PLUGIN")
+PLUGIN = os.getenv("NAPARI_PLUGIN")
 if not PLUGIN:
     raise ValueError(
-        "This test requires that you specify a plugin with the PLUGIN env var"
+        "This test requires that you specify a plugin with the NAPARI_PLUGIN env var"
     )
 
 
 def plugin_entrypoints(package) -> Iterable[Tuple[str, str]]:
-    p = configparser.ConfigParser()
-    eps = metadata.distribution(package).read_text("entry_points.txt")
-    if not eps:
-        return
-    p.read_string(eps)
-    if "napari.plugin" not in p:
-        return
-    yield from p["napari.plugin"].items()
+    for ep in metadata.distribution(package).entry_points:
+        if ep.group != "napari.plugin":
+            continue
+        yield (ep.name, ep.value)
 
 
 def test_plugin_detected():
     from napari.plugins import plugin_manager
 
+    with warnings.catch_warnings():
+        # don't let warnings on import fail the test
+        warnings.simplefilter("ignore")
+        for err in plugin_manager.discover()[1]:
+            print(err.format())
+
     eps = list(plugin_entrypoints(PLUGIN))
     assert eps
 
-    plugin_manager.discover()
     for name, ep in eps:
         assert name in plugin_manager.plugins
